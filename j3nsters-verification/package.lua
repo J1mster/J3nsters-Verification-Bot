f0117a4c680102ee2691949f827c1cfe4b592b21
J1mster <82846092+J1mster@users.noreply.github.com>
2024-01-24 19:15:17 -0600

    Delete j3nsters-verification directory

--- j3nsters-verification/package.lua
+++
@@ -1,9 +0,0 @@
-return {
-    name = "j3nster-s-verification/luvit-discordia",
-    version = "1.0.0",
-    description = "Created by @j3nster for Verifying and Linking Roblox accounts",
-    dependencies = {
-        "luvit/luvit",
-        "SinisterRectus/discordia"
-    }
-}
